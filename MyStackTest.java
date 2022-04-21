79508d721dad56016c8c1f847b58a9ce7830dd60
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-21 23:05:36 +0530

    Added: Test case to check push, peek, pop method

--- MyStackTest.java
+++ MyStackTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import com.bridgelabz.linkedlist.INode;
+import com.bridgelabz.linkedlist.MyLinkedList;
 import com.bridgelabz.linkedlist.MyNode;
 
 class MyStackTest {
@@ -30,5 +31,36 @@ class MyStackTest {
 		
 		//Test Condition
 		Assert.assertEquals(myThirdNode, peek);
+	}
+
+	/*
+	 * Test case to check push, peek and pop method
+	 */
+	@Test
+	public void abilityToPeekAndPopFromTheStackTillListIsEmpty() {
+		//value assigned to each node using object
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		
+		//object of MyStack class
+		MyStack myStack = new MyStack();
+		//push nodes to stack
+		myStack.push(myFirstNode);
+		myStack.push(mySecondNode);
+		myStack.push(myThirdNode);
+		myStack.printStack();
+		
+		MyLinkedList myLinkedList = new MyLinkedList();
+		
+		//peek and pop method to make list empty
+		while(!myStack.isEmpty()) {
+			myStack.peek();
+		    System.out.println(myStack.peek().getKey());
+		    myStack.pop();
+		}
+		
+		//Test condition
+		Assert.assertTrue(myStack.isEmpty());
 	}
 }
