a1603a6131e63d929caab9f252ca6b5c7ef0e119
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-21 23:04:18 +0530

    Added: Ability to peak and pop from the stack till it is empty

--- MyStack.java
+++ MyStack.java
@@ -3,29 +3,54 @@ package com.bridgelabz.stacksandqueues;
 import com.bridgelabz.linkedlist.INode;
 import com.bridgelabz.linkedlist.MyLinkedList;
 /*
- * Ability to create stack of 56->30->70
+ * Ability to peak and pop from the stack till it is empty
  */
 public class MyStack {
 	//variable
-	private final MyLinkedList myLinkedList;
+	public final MyLinkedList myLinkedList;
     
 	//constructor
 	public MyStack() {
 		this.myLinkedList = new MyLinkedList();
 	}
 	
-	//push method to add elements in stack
+	/*
+	 * push method to add elements in stack
+	 */
 	public void push(INode myNode) {
 		myLinkedList.add(myNode);
 	}
 	
-	//peek method to return top element of the stack
+	/*
+	 * peek method to return top element of the stack
+	 */
 	public INode peek() {
 		return myLinkedList.head;
 	}
 	
-	//printStack method to print the stack
+	/*
+	 * printStack method to print the stack
+	 */
 	public void printStack() {
 		myLinkedList.printMyNodes();
+	}
+	
+	/*
+	 * pop method to delete top element
+	 */
+	public void pop() {
+		myLinkedList.pop();
+	}
+	
+	/*
+	 * isEmpty method to check stack is empty or not
+	 */
+	public boolean isEmpty() {
+		if (myLinkedList.head != null) {
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
 }
