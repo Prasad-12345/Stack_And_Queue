82e3dd258ecddf6fbd33facedd6eb89141287e85
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-22 00:31:51 +0530

    Added: Test case to check dequeue method

--- MyQueueTest.java
+++ MyQueueTest.java
@@ -34,4 +34,36 @@ class MyQueueTest {
 		//Test condition
 		Assert.assertEquals(deque, myFirstNode);
 	}
+	
+	/*
+	 * Test case to check dequeue method
+	 */
+	@Test
+	public void toCheckDequeMethod() {
+		//value assigned to each node using object
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		
+		//object of MyQueue class
+		MyQueue myQueue = new MyQueue();
+		
+		//enqueue method
+		myQueue.enqueue(myFirstNode);
+		myQueue.enqueue(mySecondNode);
+		myQueue.enqueue(myThirdNode);
+		
+		//printQueue method
+		myQueue.printQueue();
+		
+		MyStack myStack = new MyStack();
+		
+		//using deque method till queue become empty
+		while(!myQueue.isEmpty()) {
+			myQueue.dequeue();
+		}
+
+		//Test condition
+		Assert.assertTrue(myQueue.isEmpty());
+	}
 }
